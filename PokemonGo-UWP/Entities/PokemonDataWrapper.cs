f1f39f03d12dcb8b21fe18c15b3fa59515f68d00
christosk92 <myproductsofficial@outlook.com>
2016-08-11 20:18:35 +0200

    Added more bindings

--- PokemonGo-UWP/Entities/PokemonDataWrapper.cs
+++ PokemonGo-UWP/Entities/PokemonDataWrapper.cs
@@ -12,6 +12,9 @@ using POGOProtos.Inventory.Item;
 using Template10.Common;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
+using PokemonGo_UWP.Assets.Pokemons;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace PokemonGo_UWP.Entities
 {
@@ -22,12 +25,29 @@ namespace PokemonGo_UWP.Entities
 
         public PokemonDataWrapper(PokemonData pokemonData)
         {
-            _pokemonData = pokemonData;            
+            _pokemonData = pokemonData;
+         
+            if (PokemonGo_UWP.Utils.PokemonInfo.GetLevel(_pokemonData) > GameClient.playerStats.Level + 1.5)
+            {
+                CpBarMax = GameClient.playerStats.Level + 1.5;
+                CpBarValue = CpBarMax;
+            }
+            else
+            {
+                CpBarMax = CpBarMax = GameClient.playerStats.Level + 1.5;
+                CpBarValue = PokemonGo_UWP.Utils.PokemonInfo.GetLevel(_pokemonData);
+            }
+            EvolvePokemonId = _pokemonData.PokemonId;
+            EvolvePokemonId++;
+
         }
 
         private DelegateCommand _gotoEggDetailsCommand;
-
+        private DelegateCommand _gotoPokemonDetails;
         public PokemonData WrappedData => _pokemonData;
+      
+
+        
 
         /// <summary>
         ///     Navigate to detail page for the selected egg
@@ -39,21 +59,35 @@ namespace PokemonGo_UWP.Entities
                 BootStrapper.Current.NavigationService.Navigate(typeof(EggDetailPage), true);
             }, () => true));
 
+
+
+        public DelegateCommand GotoPokemonDetailsCommand => _gotoPokemonDetails ?? (
+           _gotoPokemonDetails = new DelegateCommand(() =>
+           {
+               NavigationHelper.NavigationState["CurrentPokemon"] = this;
+               BootStrapper.Current.NavigationService.Navigate(typeof(PokemonView), true);
+           }, () => true));
+
         #region Wrapped Properties
 
-        public ulong Id => _pokemonData.Id;
-
         public PokemonId PokemonId => _pokemonData.PokemonId;
+
+        public PokemonId EvolvePokemonId { get; set; }
+       
+        public double CpBarMax { get; set; }
+        public double CpBarValue { get; set; }
 
         public int Cp => _pokemonData.Cp;
 
         public int Stamina => _pokemonData.Stamina;
 
-        public int StaminaMax => _pokemonData.StaminaMax;        
+        public int StaminaMax => _pokemonData.StaminaMax;
 
         public PokemonMove Move1 => _pokemonData.Move1;
 
         public PokemonMove Move2 => _pokemonData.Move2;
+
+        public ulong Id => _pokemonData.Id;
 
         public string DeployedFortId => _pokemonData.DeployedFortId;
 
@@ -67,9 +101,16 @@ namespace PokemonGo_UWP.Entities
 
         public int Origin => _pokemonData.Origin;
 
-        public float HeightM => _pokemonData.HeightM;
+        public string HeightM => _pokemonData.HeightM.ToString().Remove(4);
 
-        public float WeightKg => _pokemonData.WeightKg;
+        char[] chars = { '.' };
+
+
+        public string[] WeightKg1 => _pokemonData.WeightKg.ToString().Split(chars);
+
+        public string WeightKg2 => WeightKg1[1];
+        public string WeightKg3 => WeightKg2.Remove(2);
+        public string WeightKg => WeightKg1[0] + "." + WeightKg3;
 
         public int IndividualAttack => _pokemonData.IndividualAttack;
 
@@ -88,6 +129,7 @@ namespace PokemonGo_UWP.Entities
         public int BattlesDefended => _pokemonData.BattlesDefended;
 
         public string EggIncubatorId => _pokemonData.EggIncubatorId;
+        public string PokemonNamee => _pokemonData.PokemonId.ToString();
 
         public ulong CreationTimeMs => _pokemonData.CreationTimeMs;
 
@@ -97,7 +139,16 @@ namespace PokemonGo_UWP.Entities
 
         public int Favorite => _pokemonData.Favorite;
 
+        public string candyUrl => getCandy.getCandyUrl(type);
+
         public string Nickname => _pokemonData.Nickname;
+
+        public string bgType => "ms-appx:///Assets/Backgrounds/details_type_bg_" + enumType.getType(_pokemonData.PokemonId.ToString()) + ".png";
+
+        public string hpText => "HP " + _pokemonData.Stamina + "/" + _pokemonData.StaminaMax;
+
+        public string type => enumType.getType(_pokemonData.PokemonId.ToString());
+
 
         public int FromFort => _pokemonData.FromFort;
 
