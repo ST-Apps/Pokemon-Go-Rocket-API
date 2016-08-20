b680e2259c6bae5842fe49af6185bbfabfaa9a9c
VRocker <craigr3@hotmail.com>
2016-08-20 20:23:15 +0100

    Removed and sorted usings

--- PokemonGo-UWP/Utils/Helpers/BandHelper.cs
+++ PokemonGo-UWP/Utils/Helpers/BandHelper.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Band;
+﻿using Microsoft.Band;
+using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PokemonGo_UWP.Utils.Helpers
 {
