4e4c241a36baaff5f33c24debc5116bdc297803d
ArthurSDev <tech.windoows@hotmail.com>
2016-08-30 23:24:51 -0300

    Update and rename InfoMessage.xaml.cs to Busy.xaml.cs

--- PokemonGo-UWP/Views/Dialogs/Busy.xaml.cs
+++ PokemonGo-UWP/Views/Dialogs/Busy.xaml.cs
@@ -2,21 +2,19 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Template10.Common;
 using Template10.Controls;
-using Windows.UI.Xaml.Media;
-using Windows.UI;
 
 namespace PokemonGo_UWP.Views
 {
-    public sealed partial class InfoMessage : UserControl
+    public sealed partial class Busy : UserControl
     {
         public static readonly DependencyProperty BusyTextProperty =
-            DependencyProperty.Register(nameof(BusyText), typeof(string), typeof(InfoMessage),
+            DependencyProperty.Register(nameof(BusyText), typeof(string), typeof(Busy),
                 new PropertyMetadata("Please wait..."));
 
         public static readonly DependencyProperty IsBusyProperty =
-            DependencyProperty.Register(nameof(IsBusy), typeof(bool), typeof(InfoMessage), new PropertyMetadata(false));
+            DependencyProperty.Register(nameof(IsBusy), typeof(bool), typeof(Busy), new PropertyMetadata(false));
 
-        public InfoMessage()
+        public Busy()
         {
             InitializeComponent();
         }
@@ -40,11 +38,9 @@ namespace PokemonGo_UWP.Views
             {
                 var modal = Window.Current.Content as ModalDialog;
                 if (modal == null) return;
-                var view = modal.ModalContent as InfoMessage;
+                var view = modal.ModalContent as Busy;
                 if (view == null)
-                    modal.ModalContent = view = new InfoMessage();
-                modal.HorizontalContentAlignment = HorizontalAlignment.Stretch;
-                modal.ModalBackground = new SolidColorBrush(Colors.Transparent);
+                    modal.ModalContent = view = new Busy();
                 modal.IsModal = view.IsBusy = busy;
                 view.BusyText = text;
             });
