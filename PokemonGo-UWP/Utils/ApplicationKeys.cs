b3b86419d3dbbea7048d02ae6da1270760b40b4b
ArthurSDev <tech.windoows@hotmail.com>
2016-08-30 23:10:17 -0300

    Update ApplicationKeys.cs

--- PokemonGo-UWP/Utils/ApplicationKeys.cs
+++ PokemonGo-UWP/Utils/ApplicationKeys.cs
@@ -8,7 +8,7 @@ namespace PokemonGo_UWP.Utils
 {
     public static class ApplicationKeys
     {
-        public static string MapServiceToken = "{Bing Map API key}";
+        public static string MapServiceToken = "{zfxYUhqDCDe1HmPNylar~_ZS7RPaOX4H0KwwGFYGKPg~ApneKarrGxbboX2YvtM44vod0vki-ZQ17xlyVj3v9moEkdlXpFkvKVUcDWL0yXXO}";
         public static string HockeyAppToken = null;
         public static string[] MapBoxTokens = {};
         public static string[] MapBoxStylesLight = {};
