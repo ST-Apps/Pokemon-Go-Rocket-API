24e3ffcfa8e884752335adaf7fa4887dfd1e1cfb
Matthias Heilhecker <m.heilhecker@mh-dev.net>
2016-09-01 17:55:52 +0200

    Added new loading images. Fixed a merging bug in *.csproj

--- PokemonGo-UWP/Views/Dialogs/LoadingScreen.xaml.cs
+++ PokemonGo-UWP/Views/Dialogs/LoadingScreen.xaml.cs
@@ -24,6 +24,8 @@ namespace PokemonGo_UWP.Views
         public static readonly DependencyProperty IsBusyProperty =
             DependencyProperty.Register(nameof(IsBusy), typeof(bool), typeof(LoadingScreen), new PropertyMetadata(false));
 
+        private DispatcherTimer timer = new DispatcherTimer();
+
         public LoadingScreen()
         {
             InitializeComponent();
@@ -31,7 +33,9 @@ namespace PokemonGo_UWP.Views
 
         public LoadingScreen(SplashScreen splashScreen) {
             InitializeComponent();
-            BusyText = Utils.Resources.CodeResources.GetString("LadingMessage");
+            ComputeInfoMessage(this);
+            timer.Start();
+            BusyText = Utils.Resources.CodeResources.GetString("LoadingMessage");
             ComputeInfoMessage(this);
             IsBusy = true;
         }
@@ -66,7 +70,7 @@ namespace PokemonGo_UWP.Views
                     modal.ModalContent = view = new LoadingScreen();
                 modal.HorizontalContentAlignment = HorizontalAlignment.Stretch;
                 modal.ModalBackground = new SolidColorBrush(Colors.Transparent);
-                if (busy && !view.IsBusy) {
+                if(busy && !view.IsBusy) {
                     ComputeInfoMessage(view);
                 }
                 modal.IsModal = view.IsBusy = busy;
@@ -79,7 +83,13 @@ namespace PokemonGo_UWP.Views
             random = new Random();
             // Sets the number of different images and messages.
             // The second parameter is the number for max count + 1 of images and messages.
-            int number = random.Next(1, 3);
+            int number = random.Next(1, 7);
+            // little Workaround, because the first image is not correct sized.
+            if (number == 1) {
+                view.BackgroundImageBrush.Stretch = Stretch.Fill;
+            } else {
+                view.BackgroundImageBrush.Stretch = Stretch.UniformToFill;
+            }
             view.BackgroundImageBrush.ImageSource =
                 new BitmapImage(new Uri($"ms-appx:///Assets/Warnings/{number}.png"));
             view.InfoMessage = Utils.Resources.CodeResources.GetString($"WarningMessage{number}");
